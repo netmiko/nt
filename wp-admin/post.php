848a77ab5e62358ba9e14197c72d6020f18f6902
Aomie <103031412+its-aomi@users.noreply.github.com>
2022-06-25 09:51:58 +0530

    Update

---
+++ wp-admin/post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://netmiko.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fnetmiko%2Fwp-admin%2Fpost.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://netmiko.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fnetmiko%2Fwp-admin%2Fpost.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://netmiko.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fnetmiko%2Fwp-admin%2Fpost.php&reauth=1">https://netmiko.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fnetmiko%2Fwp-admin%2Fpost.php&reauth=1</a></p>
+	</body>
+</html>
